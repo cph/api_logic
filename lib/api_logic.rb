c8076f85fec8710ed52cedd7bd4dad3a9741cfb4
Bob Lail <bob.lail@cph.org>
2014-04-10 10:37:44 -0500

    [refactor] Refactored ApiLogic to be simpler (8m)

--- lib/api_logic.rb
+++ lib/api_logic.rb
@@ -9,6 +9,8 @@ module ApiLogic
   included do
     self.responder = ::ApiLogic::Responder
     
+    before_filter :find_model, :only => [:show, :update, :destroy]
+    
     guess_model
     
     def self.inherited(subclass)
@@ -21,7 +23,6 @@ module ApiLogic
   
   module ClassMethods
     
-    
     attr_reader :model_class
     alias :model :model_class
     attr_reader :model_collection
@@ -29,69 +30,72 @@ module ApiLogic
     
     
     def guess_model
-      # puts "[api_logic] guessing model for #{self.name}"
-      if self.name =~ /(?:.*::)(.*?)ApiController/
-        model_name = $1.singularize
-        return if model_name.blank?
-        begin
-          exposes_model model_name.constantize
-        rescue NameError
-          # puts "  there is no model \"#{model_name}\""
-        end
-      else
-        # puts "  the controller does not match the pattern /(?:.*::)(.*?)ApiController/"
-      end
+      return unless self.name =~ /(?:.*::)(.*?)ApiController/
+      model_name = $1.singularize
+      return if model_name.blank?
+      
+      exposes_model model_name.constantize
+    rescue NameError
+      Rails.logger.debug "[api_logic] there is no model named \"#{model_name}\""
     end
     
     
     def exposes_model(model_class)
-      # puts "[api_logic] exposing model #{model_class} on #{self.name}"
-      
       @model_class = model_class
-      @model_collection = model_class.name.tableize
-      @model_singular = @model_collection.singularize
-      
-      before_filter :find_model, :only => [:show, :update, :destroy]
-      
-    public
-      
-      define_method :index do
-        collection = find_models
-        instance_variable_set "@#{model_collection}", collection
-        respond_with collection
-      end
-      
-      define_method :show do
-        respond_with @model
-      end
-      
-      define_method :create do
-        @model = create_model
-        instance_variable_set "@#{model_singular}", @model
-        respond_with @model
-      end
-      
-      define_method :update do
-        @model.update_attributes(update_attributes)
-        respond_with @model
-      end
-      
-      define_method :destroy do
-        @model.destroy
-        respond_with @model
-      end
-      
+      @model_collection = model_class && model_class.name.tableize
+      @model_singular = model_collection && model_collection.singularize
     end
     
   end
   
   
+  
+  def index
+    raise NotImplementedError unless model_class
+    
+    collection = find_models
+    instance_variable_set "@#{model_collection}", collection
+    respond_with collection
+  end
+  
+  def show
+    raise NotImplementedError unless model_class
+    
+    respond_with @model
+  end
+  
+  def create
+    raise NotImplementedError unless model_class
+    
+    @model = create_model
+    instance_variable_set "@#{model_singular}", @model
+    respond_with @model
+  end
+  
+  def update
+    raise NotImplementedError unless model_class
+    
+    @model.update_attributes(update_attributes)
+    respond_with @model
+  end
+  
+  def destroy
+    raise NotImplementedError unless model_class
+    
+    @model.destroy
+    respond_with @model
+  end
+  
+  
+  
+protected
   
   def find_models
     model_class.all
   end
   
   def find_model
+    return unless model_class
     @model = model_class.find(params[:id])
     instance_variable_set "@#{model_singular}", @model
   end
@@ -99,6 +103,7 @@ module ApiLogic
   def create_model
     model_class.create(create_attributes)
   end
+  
   
   
   def create_attributes
@@ -114,6 +119,7 @@ module ApiLogic
   end
   
   
+  
   def model_class
     self.class.model
   end
@@ -125,7 +131,5 @@ module ApiLogic
   def model_singular
     self.class.model_singular
   end
-  
-  
   
 end

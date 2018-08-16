bd7a69ac40b0b1a366b7f27ebf21e6f1f8f17e85
Bob Lail <bob.lailfamily@gmail.com>
2018-08-15 20:28:17 -0500

    [skip] Normalized whitespace (1m)

--- lib/api_logic/responder.rb
+++ lib/api_logic/responder.rb
@@ -2,17 +2,17 @@ require 'action_controller/responder'
 
 module ApiLogic
   class Responder < ActionController::Responder
-    
-    
+
+
     def respond
-      
+
       # api_behavior checks for errors in Rails 3.0.3, but not in Rails
       # 3.1.0. To be sure that we behave correctly, check here.
       if has_errors? && !get?
         controller.render format => {:errors => resource.errors}, :status => :unprocessable_entity
-        
+
       else
-        
+
         # An API controller might want to tailor its response
         # with a RABL template. If a template exists, render that.
         #
@@ -27,13 +27,13 @@ module ApiLogic
         end
       end
     end
-    
-    
+
+
     def display(resource, given_options={})
       super resource, given_options.merge(:callback => controller.params[:callback])
     end
-    
-    
+
+
   end
 end
 
